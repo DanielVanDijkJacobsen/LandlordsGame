f9da925b5619c3567e2efd28a2e4fb4fe63f78cb
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-11-25 10:40:59 +0100

    JSON Importer fix
    JSON importer burde nu kunne læse filer. Så skal vi bare bruge dataen til at lave ting

--- src/DataHandler/JSONImporter.java
+++ src/DataHandler/JSONImporter.java
@@ -3,19 +3,25 @@ package DataHandler;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
+//Følger denne guide https://howtodoinjava.com/library/json-simple-read-write-json-examples/
 
 public class JSONImporter {
     JSONParser parser = new JSONParser();
-
-    try (FileReader reader = new FileReader("DataHandler/package.json")) {
-        Object object = parser.parse(reader);
-
-        JSONArray plots = (JSONArray) object;
-        for (int i = 0; i < plots.size(); i++) {
-
+    FileReader reader;
+    {
+        try {
+            reader = new FileReader("DataHandler/package.json");
+            Object object = parser.parse(reader);
+            JSONArray listOfPlots = (JSONArray) object;
+            System.out.println(listOfPlots);
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
         }
     }
+
 }
