06c104080ce8f27ae9b4bdb7f1093381672e9028
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-11-26 12:55:55 +0100

    Revert "Update GUI"
    This reverts commit b1b15ec0585533c1f01ff5bbbfb2d12052114550.

--- src/Game/GameMaster.java
+++ src/Game/GameMaster.java
@@ -27,15 +27,5 @@ public class GameMaster implements Serializable {
         this.endOnOverwhelmingWealth = endOnOverwhelmingWealth;
         this.differingInheritance = differingInheritance;
         this.randomizeTurnOrder = randomizeTurnOrder;
-
-        players.add<Player1>;
-        players.add<Player2>;
-        players.add<Player3>;
-        players.add<Player4>;
-        players.add<Player5>;
-        players.add<Player6>;
-
-      //  dataPlayer1;
     }
-
 }
