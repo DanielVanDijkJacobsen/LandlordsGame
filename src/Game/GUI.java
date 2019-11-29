3ffe47360b243ec93fb060af26d7c6f068d5458d
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-11-29 14:46:15 +0100

    Turn order function
    Changing turns

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -15,18 +15,19 @@ public class GUI {
 
     @FXML private Button rollDice;
 
-    @FXML private Circle playerToken1; @FXML private Circle playerToken2; @FXML private Circle playerToken3;
-    @FXML private Circle playerToken4; @FXML private Circle playerToken5; @FXML private Circle playerToken6;
+    @FXML private Group playerTokens;
 
     @FXML private Group plots;
 
     @FXML
     private void rollDiceAction(Event event) {
-        theGame.throwDiceToMove(theGame.players.get(0));
-        if (theGame.players.get(0).boardPosition > 40) {
-            theGame.players.get(0).boardPosition = theGame.players.get(0).boardPosition -40;
+        theGame.throwDiceToMove(theGame.players.get(theGame.turnOrder));
+        System.out.println(theGame.turnOrder + " " + theGame.players.get(theGame.turnOrder).boardPosition);
+        if (theGame.players.get(theGame.turnOrder).boardPosition > 40) {
+            theGame.players.get(theGame.turnOrder).boardPosition = theGame.players.get(theGame.turnOrder).boardPosition -40;
         }
-        playerToken1.setLayoutY(plots.getChildren().get(theGame.players.get(0).boardPosition).getLayoutY());
-        playerToken1.setLayoutX(plots.getChildren().get(theGame.players.get(0).boardPosition).getLayoutX());
+        playerTokens.getChildren().get(theGame.turnOrder).setLayoutY(plots.getChildren().get(theGame.players.get(theGame.turnOrder).boardPosition).getLayoutY());
+        playerTokens.getChildren().get(theGame.turnOrder).setLayoutX(plots.getChildren().get(theGame.players.get(theGame.turnOrder).boardPosition).getLayoutX());
+        theGame.endTurn();
     }
 }
