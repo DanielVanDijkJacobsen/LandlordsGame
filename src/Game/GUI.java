a831a4d13af75b25f2c3f12cdbde39d82d8ba90b
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-11-29 20:28:52 +0100

    GUI update
    Viser nu player Statistics efter hvert slag.

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -4,11 +4,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import javafx.scene.control.Label;
 
 public class GUI {
     public GameMaster theGame;
@@ -16,6 +12,12 @@ public class GUI {
     @FXML private Button rollDice;
 
     @FXML private Group playerTokens;
+    @FXML private Label playerName1; @FXML private Label playerWealth1; @FXML private Label playerPosition1;
+    @FXML private Label playerName2; @FXML private Label playerWealth2; @FXML private Label playerPosition2;
+    @FXML private Label playerName3; @FXML private Label playerWealth3; @FXML private Label playerPosition3;
+    @FXML private Label playerName4; @FXML private Label playerWealth4; @FXML private Label playerPosition4;
+    @FXML private Label playerName5; @FXML private Label playerWealth5; @FXML private Label playerPosition5;
+    @FXML private Label playerName6; @FXML private Label playerWealth6; @FXML private Label playerPosition6;
 
     @FXML private Group plots;
 
@@ -28,6 +30,28 @@ public class GUI {
         }
         playerTokens.getChildren().get(theGame.turnOrder).setLayoutY(plots.getChildren().get(theGame.players.get(theGame.turnOrder).boardPosition).getLayoutY());
         playerTokens.getChildren().get(theGame.turnOrder).setLayoutX(plots.getChildren().get(theGame.players.get(theGame.turnOrder).boardPosition).getLayoutX());
+        updatePlayerStatistics();
         theGame.endTurn();
+    }
+
+    private void updatePlayerStatistics() {
+        this.playerName1.setText(theGame.players.get(0).name);
+        this.playerName2.setText(theGame.players.get(1).name);
+        this.playerName3.setText(theGame.players.get(2).name);
+        this.playerName4.setText(theGame.players.get(3).name);
+        this.playerName5.setText(theGame.players.get(4).name);
+        this.playerName6.setText(theGame.players.get(5).name);
+        this.playerWealth1.setText(theGame.players.get(0).wealth + ",-");
+        this.playerWealth2.setText(theGame.players.get(1).wealth + ",-");
+        this.playerWealth3.setText(theGame.players.get(2).wealth + ",-");
+        this.playerWealth4.setText(theGame.players.get(3).wealth + ",-");
+        this.playerWealth5.setText(theGame.players.get(4).wealth + ",-");
+        this.playerWealth6.setText(theGame.players.get(5).wealth + ",-");
+        this.playerPosition1.setText("Placement: " + theGame.players.get(0).boardPosition);
+        this.playerPosition2.setText("Placement: " + theGame.players.get(1).boardPosition);
+        this.playerPosition3.setText("Placement: " + theGame.players.get(2).boardPosition);
+        this.playerPosition4.setText("Placement: " + theGame.players.get(3).boardPosition);
+        this.playerPosition5.setText("Placement: " + theGame.players.get(4).boardPosition);
+        this.playerPosition6.setText("Placement: " + theGame.players.get(5).boardPosition);
     }
 }
