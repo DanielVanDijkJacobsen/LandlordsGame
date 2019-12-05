1069dace4a5e0d7dccb5876340c50939f23defcb
sgerder <55347480+sgerder@users.noreply.github.com>
2019-12-05 14:53:04 +0100

    changed confirmation dialog to information dialog.

--- src/TestFeature/AsgerTemp.java
+++ src/TestFeature/AsgerTemp.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
@@ -16,10 +15,8 @@ public class AsgerTemp {
     Stage window;
     Scene scene1, scene2;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
-        Button btn1 = new Button("LALALA");
+    //@Override
+    public void confirmationDialog(Stage primaryStage) throws Exception {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation Alert");
         alert.setHeaderText("Do you wish to buy this property?");
@@ -38,51 +35,43 @@ public class AsgerTemp {
     }
 
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
-        Button btn1 = new Button("LALALA");
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Payment Alert");
-        alert.setHeaderText("You have landed on a property, pay up!");
+    //@Override
+    public void payDialog(Stage primaryStage) throws Exception {
+        Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+        alert1.setTitle("Payment Alert");
+        alert1.setHeaderText("You have landed on a property, pay up!"); //evt indsæt beløb der skal betales
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {  //skal kun have en ok knap, skal rettes til.
-            // user picks okay
+        Optional<ButtonType> result = alert1.showAndWait();
+        if (result.get() == ButtonType.OK) {
+
         } else {
-            // user picks cancel
+
         }
 
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
-        //Button btn1 = new Button("LALALA");
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("You have lost! Goodbye!");
-        alert.setHeaderText("Better luck next time!");
+    //@Override
+    public void lostDialog(Stage primaryStage) throws Exception {
+        Alert alert2 = new Alert(Alert.AlertType.INFORMATION);
+        alert2.setTitle("Information Alert");
+        alert2.setHeaderText("You have lost" );
+        alert2.setContentText("Better luck next time =)");
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {    //skal kun have ok knap, mangler at modificeres
-            // user picks okay
+        Optional<ButtonType> result = alert2.showAndWait();
+        if(result.get() == ButtonType.OK) {
+
         } else {
-            // user picks cancel
         }
     }
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
-        //Button btn1 = new Button("LALALA");
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("You have landed on a chance card!");
-        alert.setHeaderText(""); //mulighed for at inserte chance card text her
+    //@Override
+    public void chanceDialog(Stage primaryStage) throws Exception {
+        Alert alert3 = new Alert(Alert.AlertType.INFORMATION);
+        alert3.setTitle("You have landed on a chance card!");
+        alert3.setHeaderText(""); //mulighed for at inserte chance card text her
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK) {   //skal finde på en anden mulighed her da user ikke skal trykke cancel på chancecards
-            // user picks okay
+        Optional<ButtonType> result = alert3.showAndWait();
+        if (result.get() == ButtonType.OK) {
         } else {
-            // user picks cancel
         }
     }
 }
