7a3f2cc8a76049f84427eec6e8e576927dac9977
DanielVanDijkJacobsen <danilar@ruc.dk>
2019-12-06 13:04:24 +0100

    Update TaxPay.java

--- src/Game/PopUpMessage/TaxPay.java
+++ src/Game/PopUpMessage/TaxPay.java
@@ -9,17 +9,31 @@ import java.util.Optional;
 
 public class TaxPay {
 
-    public TaxPay(Player player) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Tax.");
-        alert.setHeaderText(player.name + " you must either pay 10% of " + player.value +
-                " which is the value of your assets or 4000,-?");
-        alert.setContentText("Will you pay to 10%?");
-        Optional<ButtonType> result = alert.showAndWait();
-        if(result.get() == ButtonType.OK) {
-            player.wealth = player.wealth - (player.value/10);
+    public TaxPay(Player player, String taxType) {
+        if (taxType.contentEquals("TAX4000/10%")) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Tax.");
+            alert.setHeaderText(player.name + " you must either pay 10% of " + player.value +
+                    " which is the value of your assets or 4000,-?");
+            alert.setContentText("Will you pay to 10%?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                player.wealth = player.wealth - (player.value / 10);
+            } else {
+                player.wealth = player.wealth - 4000;
+            }
+        } else if (taxType.contentEquals("TAX2000")) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Tax.");
+            alert.setHeaderText(player.name + " you must either pay 2000,- in taxes.");
+            alert.setContentText("Press to pay, you have no choice.");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.get() == ButtonType.OK) {
+                player.wealth = player.wealth - 2000;
+            } else {
+            }
         } else {
-            player.wealth = player.wealth -4000;
+            System.out.println("Unknown tax type.");
         }
     }
 }
